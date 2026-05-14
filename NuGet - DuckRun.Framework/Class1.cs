cbc83e1f802df86dde3625177089166139278dd9
Kyle Müller <kyle.mueller@astrava.de>
2026-05-14 13:23:20 +0200

    Add NuGet - DuckRun.Framework project and update solution
    Updated DuckRun.slnx to fix DuckRun.Core path and add the new NuGet - DuckRun.Framework project targeting .NET Framework 4.8. Re-added DuckRun.Core files to address formatting. Added initial Class1.cs and AssemblyInfo.cs for the new framework project.

---
+++ NuGet - DuckRun.Framework/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet___DuckRun.Framework
+{
+    public class Class1
+    {
+    }
+}
